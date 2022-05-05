b7ae50b91490b1812593b2cd27c0c4ebd3bdb3db
CristinaSilvan <cristina_silvan@outlook.es>
2022-05-05 16:22:15 +0200

    Actualización ejercicios

--- Ejercicios de Clase/Ejercicios de Colecciones/Ejercicio 1.py
+++ Ejercicios de Clase/Ejercicios de Colecciones/Ejercicio 1.py
@@ -1,1 +1,14 @@
-import random
+'''
+Hacer un programa que procese 100 mujeres y hombres (M/H).
+Mostrar el porcentaje de hombres y mujeres y si hay más mujeres que hombres, si hay igualdad o si hay más hombres que mujeres.
+'''
+
+from random import randint
+
+# En este programa, procesa las mujeres como 0 y los hombres como 1
+lista = []
+for i in range(0,100):
+    lista.append(randint(0,1))
+
+print(f"El porcentaje de mujeres es del: {lista.count(0)}%")
+print(f"El porcentaje de hombres es del: {lista.count(1)}%")
---
+++ Ejercicios de Clase/Ejercicios de Colecciones/Ejercicio 2.py
@@ -0,0 +1,42 @@
+'''
+Hacer un programa que procese las edades de 100 personas. 
+Deberá decir cuantas tienen más de (≥18) y cuál es la persona con menor edad 
+y cuál es la persona con mayor edad. También deberá mostrar el porcentaje de edades de las 100 personas.
+'''
+
+from random import randint
+
+lista = []
+for i in range(0,100):
+    lista.append(randint(1,115))
+
+#print(lista)
+mayores_edad = 0
+for i in lista:
+    if i > 18:
+        mayores_edad += 1
+por1 = 0.00
+por2 = 0.00
+por3 = 0.00
+por4 = 0.00
+por5 = 0.00
+por6 = 0.00
+for i in lista:
+    if i < 18:
+        por1+=1
+    elif i < 35:
+        por2+=1
+    elif i < 50:
+        por3+=1
+    elif i < 75:
+        por4+=1
+    elif i < 90:
+        por5+=1
+    else:
+        por6+=1
+print("Total de edades: {}\n".format(lista))
+print("Hay {} personas mayores de edad".format(mayores_edad))
+print("La persona más joven tiene {} años".format(min(lista)))
+print("La persona más mayor tiene {} años".format(max(lista)))
+print("""Porcentajes de edad: \n\tDe 1 a 18 años: {}%\n\tDe 18 a 35 años: {}%\n\tDe 35 a 50 años: {}%\n\tDe 50 a 75 años: {}%\n\tDe 75 a 90 años: {}%\n\tDe 90 a 115 años: {}%"""
+.format((round((por1*100)/115)),(round((por2*100)/115)),(round((por3*100)/115)),(round((por4*100)/115)),(round((por5*100)/115)),(round((por6*100)/115))))
