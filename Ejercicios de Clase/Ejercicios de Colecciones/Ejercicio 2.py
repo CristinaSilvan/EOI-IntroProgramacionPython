a59a1a51481b33c1f0f2fcb28a9c4d544554b812
CristinaSilvan <cristina_silvan@outlook.es>
2022-05-06 23:26:43 +0200

    Actualización ejercicios

--- Ejercicios de Clase/Ejercicios de Colecciones/Ejercicio 2.py
+++ Ejercicios de Clase/Ejercicios de Colecciones/Ejercicio 2.py
@@ -8,6 +8,29 @@ from random import randint
 
 lista = []
 for i in range(0,100):
+    lista.append(randint(1, 120))
+
+lista.sort()
+# print(lista)
+for i in lista:
+    if i >= 18:
+        print(f"Hay {len(lista[lista.index(i):])} personas mayores de edad")
+        break
+print(f"La persona más joven tiene {min(lista)} años y la más mayor {max(lista)} años\n")
+
+for i in lista:
+    print(f"El porcentaje de personas con {i} años es de {round((((lista.count(i))*100)/120),2)}%")
+    for j in range(lista.index(i),len(lista)-1):
+        if j == i:
+            lista.pop(j)
+
+'''
+# Versión antigua
+
+from random import randint
+
+lista = []
+for i in range(0,100):
     lista.append(randint(1,115))
 
 #print(lista)
@@ -40,3 +63,4 @@ print("La persona más joven tiene {} años".format(min(lista)))
 print("La persona más mayor tiene {} años".format(max(lista)))
 print("""Porcentajes de edad: \n\tDe 1 a 18 años: {}%\n\tDe 18 a 35 años: {}%\n\tDe 35 a 50 años: {}%\n\tDe 50 a 75 años: {}%\n\tDe 75 a 90 años: {}%\n\tDe 90 a 115 años: {}%"""
 .format((round((por1*100)/115)),(round((por2*100)/115)),(round((por3*100)/115)),(round((por4*100)/115)),(round((por5*100)/115)),(round((por6*100)/115))))
+'''
