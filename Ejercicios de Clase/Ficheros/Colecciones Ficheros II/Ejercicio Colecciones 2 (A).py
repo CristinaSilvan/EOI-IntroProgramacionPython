ec2a220cc4eb07e4971e9a8b805c4927304093b3
CristinaSilvan <cristina_silvan@outlook.es>
2022-05-16 20:45:02 +0200

    Actualización repositorio

--- Ejercicios de Clase/Ficheros/Colecciones Ficheros II/Ejercicio Colecciones 2 (A).py
+++ Ejercicios de Clase/Ficheros/Colecciones Ficheros II/Ejercicio Colecciones 2 (A).py
@@ -15,16 +15,10 @@ def lee_o_crea_fichero(file, personas):
     finally:
         fichero.close()
 
-print(f"Ejercicio: {__name__}")
+#print(f"Ejercicio: {__name__}")
 if __name__ == '__main__': # Esto es para diferenciar la función del código principal, para poder usar la función fuera del módulo sin problemas
-    personas1 = []
-    for n in range(1,101):
-        personas1.append(randint(0,125))
+    personas = [randint(1,125) for i in range(1,101)]
 
-    personas2 = [randint(1,125) for i in range(1,101)]
+    file = '.\\Ejercicios de Clase\\Ficheros\\Colecciones Ficheros II\\Datos_2 (A).txt'
 
-    file1 = '.\\Ejercicios de Clase\\Ficheros\\Colecciones Ficheros II\\Datos_Colecciones2 (1).txt'
-    file2 = '.\\Ejercicios de Clase\\Ficheros\\Colecciones Ficheros II\\Datos_Colecciones2 (2).txt'
-
-    lee_o_crea_fichero(file1,personas1)
-    lee_o_crea_fichero(file2,personas2)
+    lee_o_crea_fichero(file,personas)
---
+++ Ejercicios de Clase/Ficheros/Colecciones Ficheros II/Ejercicio Colecciones 2 (B).py
@@ -0,0 +1,29 @@
+fichero = "C:\\00-Python\\Ejercicios de Clase\\Ficheros\\Colecciones Ficheros II\\Datos_2 (A).txt"
+archivo = open(fichero, "rt", encoding='UTF-8')
+
+cadena = archivo.read()
+cadena = cadena.strip()
+cadena = cadena.strip('[')
+cadena = cadena.strip(']')
+lista = [int(x) for x in cadena.split(',')]
+
+archivo.close()
+fichero = "C:\\00-Python\\Ejercicios de Clase\\Ficheros\\Colecciones Ficheros II\\Datos_2 (B).txt"
+archivo = open(fichero, "wt", encoding='UTF-8')
+
+archivo.write("Lista generada: {}".format(str(lista)))
+
+lista.sort()
+for i in lista:
+    if i >= 18:
+        archivo.write(f"Hay {len(lista[lista.index(i):])} personas mayores de edad\n")
+        break
+archivo.write(f"La persona más joven tiene {min(lista)} años y la más mayor {max(lista)} años\n\n")
+
+for i in lista:
+    archivo.write(f"El porcentaje de personas con {i} años es de {round(((lista.count(i))),2)}%\n")
+    for j in range(lista.index(i),len(lista)-1):
+        if j == i:
+            lista.pop(j)
+
+archivo.close()
