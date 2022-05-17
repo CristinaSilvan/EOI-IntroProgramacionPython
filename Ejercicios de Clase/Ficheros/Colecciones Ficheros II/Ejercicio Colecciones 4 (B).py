be6b426e83875333081b68c50cb891eb8fff332b
CristinaSilvan <cristina_silvan@outlook.es>
2022-05-17 10:23:34 +0200

    Actualización repositorio

--- Ejercicios de Clase/Ficheros/Colecciones Ficheros II/Ejercicio Colecciones 4 (B).py
+++ Ejercicios de Clase/Ficheros/Colecciones Ficheros II/Ejercicio Colecciones 4 (B).py
@@ -10,6 +10,8 @@ archivo = open(file, "wt", encoding='UTF-8')
 
 archivo.write("RESULTADOS DEL EJERCICIO 4:\n\n")
 
+archivo.write("="*100+'\n')
+
 DiccPromedioAnual={} 
 for clave,valor in dicc.items():
     archivo.write(f'{clave} -> {valor} \t')
@@ -23,11 +25,14 @@ archivo.write(f'La ciudad con el promedio anual mas ALTO 1 es {list(DiccPromedio
 archivo.write(f'\nLa ciudad con el promedio anual mas ALTO 2 es {max(DiccPromedioAnual,key=DiccPromedioAnual.get)} con un promedio de: {MaxProAnual} ºC')
  
 archivo.write(f'\n\nLa ciudad con el promedio anual mas BAJO 1 es {list(DiccPromedioAnual.keys())[list(DiccPromedioAnual.values()).index(MinProAnual)]} con un promedio de: {MinProAnual} ºC') 
-archivo.write(f'\nLa ciudad con el promedio anual mas BAJO 2 es {min(DiccPromedioAnual,key=DiccPromedioAnual.get)} con un promedio de: {MinProAnual} ºC')
+archivo.write(f'\nLa ciudad con el promedio anual mas BAJO 2 es {min(DiccPromedioAnual,key=DiccPromedioAnual.get)} con un promedio de: {MinProAnual} ºC\n')
 ListaPromedioAnual = list(DiccPromedioAnual.items())
 ListaPromedioAnual.sort(key = lambda x: x[1], reverse=True)
-archivo.write(f'\n\nLa lista de las ciudades ordenadas es (1) :\n{ListaPromedioAnual}\n\n')
+archivo.write("="*100+'\n')
+archivo.write(f'\n\nLa lista de las ciudades ordenadas es :\n{ListaPromedioAnual}\n\n')
 ListaPromedioAnual1 = sorted(DiccPromedioAnual,key=DiccPromedioAnual.get,reverse=True)
+
+archivo.write("="*100+'\n\n')
 
 for i in ListaPromedioAnual1:
     archivo.write(i + " = ")
