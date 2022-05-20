65bd93f27bc4fc8569c0bd6eed8e6dd985145ffa
CristinaSilvan <cristina_silvan@outlook.es>
2022-05-20 17:42:52 +0200

    Actualización repositorio

--- Ejercicios de Clase/Base de Datos/src/Estado_Mongo.py
+++ Ejercicios de Clase/Base de Datos/src/Estado_Mongo.py
@@ -8,7 +8,7 @@ print('Version: ',resultado['version'])
 print('Proceso: ',resultado['process'])
 
 # Guardado de resultado en .txt
-Resultado = 'Host: '+ resultado['host'] + '\n' + 'Version: ' + resultado['version'] + '\n' + 'Proceso: ' + resultado['process']
+Resultado_terminal = 'Host: '+ resultado['host'] + '\n' + 'Version: ' + resultado['version'] + '\n' + 'Proceso: ' + resultado['process']
 fichero = open('C:\\00-Python\\Ejercicios de Clase\\Base de Datos\\Resultado_Prueba_Conexión.txt', 'wt', encoding='UTF-8')
-fichero.write(str(Resultado))
+fichero.write(str(Resultado_terminal))
 fichero.close()
