ba434a7b8797e3354217906dbaa3cae4dfe3c98d
CristinaSilvan <cristina_silvan@outlook.es>
2022-05-25 20:43:43 +0200

    Actualización repositorio

--- Ejercicios de Clase/Bases de Datos/MongoDB/src/Ejercicios/MONGODB_Colecciones_4.py
+++ Ejercicios de Clase/Bases de Datos/MongoDB/src/Ejercicios/MONGODB_Colecciones_4.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from os.path import exists
 from random import randint
 
-
+# Lee el fichero, sino existe lo crea
 def GestionFichero(fichero_nombre):
     try:
         if exists(fichero_nombre):
@@ -21,6 +21,7 @@ def GestionFichero(fichero_nombre):
 
     return datos
 
+# Función auxiliar que crea el fichero y lo rellena
 def RellenarFichero ():
     ListaCiudades = ['Malaga','Alava','Albacete','Alicante','Almería','Asturias','Avila','Badajoz','Barcelona','Burgos','Cáceres','Cádiz','Cantabria','Castellón','Ceuta','Ciudad Real','Córdoba','Cuenca','Formentera','Girona']
     dicc = {}
@@ -32,23 +33,31 @@ def RellenarFichero ():
     dicc[ciudad] = listatemperaturas
     return dicc
 
-
+# Trabajo con la Base de Datos
 if __name__=='__main__':
     nombre_fichero = ".\Ejercicios de Clase\Ficheros\Colecciones Ficheros II\Datos_4 (A).txt"
     datos_fichero = eval(GestionFichero(nombre_fichero))
-    #print(type(datos_fichero))
 
     url = "mongodb://localhost:27017/"
     conexion = MongoClient(url) # Establecer conexión
     cursor = conexion.admin # Crear cursor o puntero
 
-    cursor = conexion["Estadisticas"] # Crendo Base de Datos
+    cursor = conexion["Estadisticas"] # Creando Base de Datos
     coleccion = cursor["ListaTemperaturas"] # Creando Colección
 
+    # Insertando información del fichero
     for i in datos_fichero:
         diccionario = {
             "Ciudad":i,
             "Temperaturas":datos_fichero[i]
         }
         coleccion.insert_one(diccionario)
+
+
+    # Leyendo información de la base de datos
+
+
+    # Procesando información y volcando resultados en fichero nuevo
+
+
     conexion.close()
